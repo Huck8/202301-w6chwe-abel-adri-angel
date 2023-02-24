2b0c3f247db3fd3572c8a879f36b483a1b96017e
Ángel <angelmonclova@gmail.com>
2023-02-24 15:24:19 +0100

    Remove src/api/students/ files and create src/api/students/ refactoring

---
+++ src/api/robots/robots-router.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import {
+  createRobotController,
+  deleteRobotByIdController,
+  getRobotByIdController,
+  getRobotsController,
+  updateRobotByIdController,
+} from './robots-controllers.js';
+
+const router = express.Router();
+
+router.route('/').get(getRobotsController).post(createRobotController);
+
+router
+  .route('/:id')
+  .get(getRobotByIdController)
+  .patch(updateRobotByIdController)
+  .delete(deleteRobotByIdController);
+
+export default router;
