43b5b8ca134fcc0c7578f8468f193aed9bfc8137
Ángel <angelmonclova@gmail.com>
2023-02-25 13:04:33 +0100

    Add gitRobotById fn

--- src/api/robots/robots-controllers.ts
+++ src/api/robots/robots-controllers.ts
@@ -12,7 +12,21 @@ export const getRobotsController: RequestHandler = async (_req, res) => {
   }
 };
 
-export const getRobotByIdController = () => {};
+export const getRobotByIdController: RequestHandler = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const robot = await RobotModel.findById(id);
+
+    if (robot === null) {
+      res.sendStatus(404);
+    } else {
+      res.json(robot);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 export const updateRobotByIdController: RequestHandler = async (req, res) => {
   const { id } = req.params;
