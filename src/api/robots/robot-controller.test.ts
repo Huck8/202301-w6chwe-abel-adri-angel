97fc47600e4995151bf11656bc8b19807d90f0c4
Ángel <angelmonclova@gmail.com>
2023-02-26 20:14:51 +0100

    Modify patch function

--- src/api/robots/robot-controller.test.ts
+++ src/api/robots/robot-controller.test.ts
@@ -1,3 +1,42 @@
-test('given a controller test', () => {
-  expect(true).toBe(true);
+import { Request, Response } from 'express';
+import { RobotModel } from './robot-schema';
+import { updateRobotByIdController } from './robots-controllers';
+
+describe('Given a getRobotsControllers function from robots-controller', () => {
+  const request = { params: { id: 'mockId' } } as Partial<Request>;
+  const response = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  } as Partial<Response>;
+
+  const robot = {
+    id: 'robotId',
+    name: 'kjhlk',
+    imageUrl: 'urlRobot',
+    velocity: 0,
+    resistance: 0,
+    creationDate: '2023-7-9',
+    faction: 'Desepticon',
+  };
+
+  test('when the database response is successfull, then it should resolve a update robot', async () => {
+    RobotModel.updateOne = jest.fn().mockResolvedValue(robot.id);
+    await updateRobotByIdController(
+      request as Request,
+      response as Response,
+      jest.fn(),
+    );
+    expect(response.sendStatus).toHaveBeenCalledWith(204);
+  });
+  test('when the database throws an error then it should respond with status 500', async () => {
+    RobotModel.find = jest
+      .fn()
+      .mockRejectedValue(new Error('something was wrong'));
+    await updateRobotByIdController(
+      request as Request,
+      response as Response,
+      jest.fn(),
+    );
+    expect(response.status).toHaveBeenCalledWith(500);
+  });
 });
