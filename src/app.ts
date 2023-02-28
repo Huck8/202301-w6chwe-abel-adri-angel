853fc67f2d72d51765a0dd90e0c0eda9461940d8
Abel <abelguevaralanda@yahoo.es>
2023-02-28 18:41:09 +0100

    add auth folders

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import apiRouter from './api/api-router.js';
+import authRouter from './api/auth/auth-router.js';
 
 const app = express();
 app.use(cors());
@@ -21,6 +22,7 @@ app.get('/', (req, res) => {
   res.json({ hello: 'World' });
 });
 app.use(express.json());
+app.use('/auth', authRouter);
 app.use('/api/v1', apiRouter);
 
 export default app;
